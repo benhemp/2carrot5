d6a17adc834870308cb191f84b5fc856711d661a
Ben Hemphill <benhemp@gmail.com>
2016-09-15 17:35:21 -0500

    Update every90.sh

--- every90.sh
+++ every90.sh
@@ -2,4 +2,4 @@
 while true; do #First Hand is always helping children
   echo "First Hand helps another child." #echo to screen the happy event
   sleep 5400 #wait 90 minutes for the next happy event
-done
+done #cerner_2^5_2016
